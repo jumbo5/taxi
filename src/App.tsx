595c43b1cc9fe40f195b78ba4c0fcd2d67960118
Юрий Лисецкий <sukmikehok@MacBook-Pro-Urij-2.local>
2021-01-05 16:36:42 +0700

    Eject

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import logo from "./logo.svg";
-
-import "./App.css";
-
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -23,6 +19,4 @@ function App() {
       </header>
     </div>
   );
-}
-
-export default App;
+};
